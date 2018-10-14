80dd8e5eb6881dd18a81a569376146dd1897aa16
Diyaa Hamza <z.t0@hotmail.com>
2018-10-14 16:12:20 +0300

    support swift 4.2

--- LocalizationHelperDemo/LocalizationHelperDemo/AppDelegate.swift
+++ LocalizationHelperDemo/LocalizationHelperDemo/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MOLHResetable {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         MOLH.shared.activate(true)
         MOLH.shared.specialKeyWords = ["Cancel","Done"]
