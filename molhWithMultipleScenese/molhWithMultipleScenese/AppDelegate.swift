837475a5d8977ae1e40f64858785bb24c28d94d1
moath othman <dark2torch@gmail.com>
2020-04-25 21:03:54 +0300

    adding multiple scenese sample

---
+++ molhWithMultipleScenese/molhWithMultipleScenese/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.swift
+//  molhWithMultipleScenese
+//
+//  Created by Moath_Othman on 4/25/20.
+//  Copyright © 2020 moath. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, MOLHResetable {
+    
+    
+    func reset() {
+         let rootviewcontroller: UIWindow = ((UIApplication.shared.delegate?.window)!)!
+         let stry = UIStoryboard(name: "Main", bundle: nil)
+         rootviewcontroller.rootViewController = stry.instantiateViewController(withIdentifier: "rootnav")
+     }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        MOLHFont.shared.arabic = UIFont(name: "Courier", size: 13)!
+        MOLHLanguage.setDefaultLanguage("ar")
+        MOLH.shared.activate(true)
+        MOLH.shared.specialKeyWords = ["Cancel","Done"]
+        
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    
+}
+
